1eab9c280088f99060130c36b3e7ef20763df20d
Shakil <theshakil53@gmail.com>
2023-05-30 18:09:02 +0600

    add user delete method

--- controller/loginController.js
+++ controller/loginController.js
@@ -2,6 +2,18 @@ function getLogin(req,res,next){
     res.render("index");
 }
 
+//do login
+async function login(req,res,next){
+    try {
+        //find a user who has this email/username
+        const user = await User.findOne({
+            $or: [{email: req.body.username}, {mobile: req.body.username}]
+        })
+    } catch (error) {
+        
+    }
+}
+
 module.exports = {
     getLogin,
 }
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 
 //INTERnal imports
-const {getUsers, addUser} = require("../controller/usersController");
+const {getUsers, addUser,removeUser} = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
 const avatarUpload = require("../middlewares/users/avatarUpload");
 const { addUserValidators, addUserValidationHandler } = require("../middlewares/users/userValidator");
@@ -16,5 +16,8 @@ router.get("/",decorateHtmlResponse("Users"), getUsers);
 
 //add user
 router.post("/", avatarUpload, addUserValidators , addUserValidationHandler,addUser);
+
+//delete user
+router.delete("/:id",removeUser)
 
 module.exports = router;
