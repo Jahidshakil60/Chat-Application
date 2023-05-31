783b54ac99b9718223d4e6e1e9176fffa6a891d7
Shakil <theshakil53@gmail.com>
2023-05-31 18:09:37 +0600

    add login logout functionality

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,12 +2,19 @@
 const express = require("express");
 
 //INTERnal imports
-const {getLogin} = require("../controller/loginController")
+const {getLogin, login, logout} = require("../controller/loginController")
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const { doLoginValidators, doLoginValidationHandler } = require("../middlewares/login/loginValidators");
+const { redirectLoggedIn } = require("../middlewares/common/checkLogin");
 
 const router = express.Router();
 
 //login page
-router.get("/",decorateHtmlResponse("Login"), getLogin);
+router.get("/",decorateHtmlResponse("Login"),redirectLoggedIn, getLogin);
+
+//process login
+router.post("/",decorateHtmlResponse("Login"), doLoginValidators, doLoginValidationHandler, login);
+
+router.delete("/", logout)
 
 module.exports = router;
